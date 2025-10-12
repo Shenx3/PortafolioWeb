c3e52cd2381a220619521c5b7eff5258da4376e1
Shenx3 <bas.bravot@duocuc.cl>
2025-10-12 19:23:56 -0300

    Subida de portafolio a GitHub

--- src/components/data/certificatesData.ts
+++ src/components/data/certificatesData.ts
@@ -1,27 +1,24 @@
-// src/data/certificatesData.ts
-
-interface Certificate {
-  title: string;
-  issuer: string;
-  date: string;
-  credentialUrl: string;
-  imageUrl: string; // <-- Agregamos esta propiedad
+interface Certificate { // Definimos la interfaz Certificate
+  title: string; // Título del certificado
+  issuer: string; // Emisor del certificado
+  date: string; // Fecha del certificado
+  credentialUrl: string; // URL del certificado
+  imageUrl: string; // Ruta de la imagen
 }
 
-export const certificatesData: Certificate[] = [
-  {
-    title: 'Artificial Intelligence Professional Certification - CAIPC®',
-    issuer: 'Certiprof',
-    date: 'Octubre 2025',
-    credentialUrl: 'https://www.credly.com/badges/0d50fabd-dca1-466a-bc24-50b7075811e3/public_url',
-    imageUrl: 'src/assets/ia-certificate.png', // <-- Ruta de la imagen
+export const certificatesData: Certificate[] = [ // Exportamos un array de certificados
+  { // Primer certificado
+    title: 'Artificial Intelligence Professional Certification - CAIPC®', // Título del certificado
+    issuer: 'Certiprof', // Emisor del certificado
+    date: 'Octubre 2025', // Fecha del certificado
+    credentialUrl: 'https://www.credly.com/badges/0d50fabd-dca1-466a-bc24-50b7075811e3/public_url', // URL del certificado
+    imageUrl: 'src/assets/ia-certificate.png', // Ruta de la imagen
   },
-  {
+  { // Segundo certificado
     title: 'Python Essentials 1 Certification - CISCO®',
-    issuer: 'Cisco',
+    issuer: 'Cisco', 
     date: 'Junio 2024',
     credentialUrl: 'https://www.credly.com/badges/014075ff-aa25-4c5a-b2bc-eed163ec47f9/public_url',
-    imageUrl: 'src/assets/python.png', // <-- Ruta de la imagen
+    imageUrl: 'src/assets/python.png',
   },
-  // Añade más objetos de certificado aquí
 ];
--- src/components/data/experienceData.ts
+++ src/components/data/experienceData.ts
@@ -1,27 +1,26 @@
-
-interface Experience {
-  title: string;
-  company: string;
-  duration: string;
-  description: string;
-  link?: string;
+interface Experience { // Definimos la interfaz Experience
+  title: string; // Título del puesto
+  company: string; // Nombre de la empresa
+  duration: string; // Duración del empleo
+  description: string; // Descripción del puesto
+  link?: string; // Enlace a la empresa o proyecto (opcional)
 }
 
-export const experienceData: Experience[] = [
-  {
-    title: 'Operador de logística',
-    company: 'Decathlon',
-    duration: 'Septiembre 2023 - Febrero 2024',
-    description: 'Team Player Logistica 40 HRS',
-    link: 'https://decathlonchile.gupy.io/',
+export const experienceData: Experience[] = [ // Exportamos un array de experiencias
+  { // Primera experiencia
+    title: 'Operador de logística', // Título del puesto
+    company: 'Decathlon', // Nombre de la empresa
+    duration: 'Septiembre 2023 - Febrero 2024', // Duración del empleo
+    description: 'Team Player Logistica 40 HRS', // Descripción del puesto
+    link: 'https://decathlonchile.gupy.io/', // Enlace a la empresa o proyecto (opcional)
   },
-  {
+  { // Segunda experiencia
     title: 'Técnico de fibra óptica',
     company: 'Grupo Schutz S.A',
     duration: 'Mayo 2023 - Agosto 2023',
     description: 'Responsable de la instalación, mantenimiento y reparación de redes de fibra óptica. Realicé pruebas de rendimiento y diagnostiqué fallas para asegurar la continuidad del servicio y optimizar la calidad de las conexiones.',
   },
-  {
+  { // Tercera experiencia
     title: 'Garzón',
     company: 'La Sanguchera del Barrio',
     duration: 'Enero 2023 - Marzo 2023',
