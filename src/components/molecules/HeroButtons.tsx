fb1bce3f8a934ebf1f756a9fc109079bd20e4294
Shenx3 <bas.bravot@duocuc.cl>
2025-10-14 12:52:43 -0300

    Actualizar rutas de imágenes en certificatesData y ajustar variantes de botones en HeroButtons para mejorar la consistencia visual.

--- src/components/molecules/HeroButtons.tsx
+++ src/components/molecules/HeroButtons.tsx
@@ -6,13 +6,13 @@ import LinkButton from '../atoms/LinkButton';
 export const HeroButtons: React.FC = () => {
   return (
     <div className="flex flex-col space-y-4 mt-6 justify-center sm:flex-row sm:space-y-0 sm:space-x-4">
-      <LinkButton to="mailto:bastianbravotobar@gmail.com" icon="email" variant="primary">
+      <LinkButton to="mailto:bastianbravotobar@gmail.com" icon="email" variant="secondary">
         Contáctame
       </LinkButton>
       <LinkButton to = "https://github.com/Shenx3" icon="github" variant="yellow">
         GitHub
       </LinkButton>
-      <LinkButton to="https://www.linkedin.com/in/bastian-bravo-17b131370/" icon="linkedin" variant="secondary">
+      <LinkButton to="https://www.linkedin.com/in/bastian-bravo-17b131370/" icon="linkedin" variant="primary">
         LinkedIn
       </LinkButton>
     </div>
