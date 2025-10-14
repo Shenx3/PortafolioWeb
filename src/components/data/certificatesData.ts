e44d3fde8b5dfa959bee39d69cb65066312f3f26
Shenx3 <bas.bravot@duocuc.cl>
2025-10-14 12:58:21 -0300

    Actualizar rutas de imágenes en certificatesData para mejorar la consistencia en la importación de recursos.

--- src/components/data/certificatesData.ts
+++ src/components/data/certificatesData.ts
@@ -1,7 +1,4 @@
 // Definimos la interfaz Certificate y exportamos un array de certificados
-import iaCertificate from '@/assets/ia-certificate.png';
-import pythonCertificate from '@/assets/python.png';
-
 interface Certificate { // Definimos la interfaz Certificate
   title: string; // Título del certificado
   issuer: string; // Emisor del certificado
@@ -16,13 +13,13 @@ export const certificatesData: Certificate[] = [ // Exportamos un array de certi
     issuer: 'Certiprof', // Emisor del certificado
     date: 'Octubre 2025', // Fecha del certificado
     credentialUrl: 'https://www.credly.com/badges/0d50fabd-dca1-466a-bc24-50b7075811e3/public_url', // URL del certificado
-    imageUrl: iaCertificate, // Ruta de la imagen
+    imageUrl: 'src/assets/ia-certificate.png', // Ruta de la imagen
   },
   { // Segundo certificado
     title: 'Python Essentials 1 Certification - CISCO®',
     issuer: 'Cisco', 
     date: 'Junio 2024',
     credentialUrl: 'https://www.credly.com/badges/014075ff-aa25-4c5a-b2bc-eed163ec47f9/public_url',
-    imageUrl: pythonCertificate,
+    imageUrl: 'src/assets/python.png',
   },
 ];
